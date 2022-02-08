89f3a890dd8441dc368eac1c0645433a37b7fccb
andrew irvine <andrewirvine3381@gmail.com>
2022-02-08 11:13:11 +0000

    added the three garages pages requires the get

---
+++ garagesdom.js
@@ -0,0 +1,9 @@
+export const selectGarage = document.getElementById(`selectGarage`);
+export const createGarage = document.getElementById(`createGarage`);
+export const delGarage = document.getElementById(`delGarage`);
+export const createButton = document.getElementById(`createButton`);
+export const listOutput = document.getElementById(`listOutput`);
+export const singleOutput = document.getElementById(`singleOutput`);
+export const inputUpdateName = document.getElementById(`inputUpdateName`);
+export const inputUpdateID = document.getElementById(`inputUpdateID`);
+export const buttonUpdate = document.getElementById(`buttonUpdate`);
